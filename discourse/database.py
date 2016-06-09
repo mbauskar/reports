e853cd49ff7c766ea6350a2d82fc529b13122f66
mbauskar <mbauskar@gmail.com>
2016-06-09 15:50:46 +0530

    [forum report] send weekly report mail

--- discourse/database.py
+++ discourse/database.py
@@ -6,15 +6,28 @@ def get_connection(user, host, passwd, db):
 	except Exception, e:
 		raise e
 
-def get_values(query, db_config):
+def get_values(query, db_config, as_dict=False):
+	_dict_result = {}
 	result = None
 	_conn = get_connection(**db_config)
 	try:
 		_cursor = _conn.cursor()
 		_cursor.execute(query)
 		result = _cursor.fetchall()
+		if result and as_dict:
+			ret = []
+			for r in result:
+				row_dict = {}
+				for i in range(len(r)):
+					val = r[i]
+					row_dict[_cursor.description[i][0]] = val
+				ret.append(row_dict)
+
+			result = ret
 	except Exception, e:
-		result = None
+		print query
+		print e
+		raise e
 	finally:
 		return result
 
--- discourse/user_activity.py
+++ discourse/user_activity.py
@@ -1,20 +1,24 @@
 import sys
 import json
 import requests
-from datetime import datetime
-from jinja2 import Environment, PackageLoader
+from mail import send_mail
 from database import get_values, set_values	
-
+from jinja2 import Environment, PackageLoader
+from datetime import datetime, date, timedelta
 
 jenv = None
 users = {}
+sender = ""
+subject = ""
+mail_pwd = ""
 base_url = ""
-activity_url = ""
-topic_url = "{base_url}/t/{slug}"
 db_config = {}
+topic_url = "{base_url}/t/{slug}"
+recipients = []
+activity_url = ""
 
-def init_report():
-	global jenv, users, base_url, activity_url, db_config
+def init_report(pwd):
+	global jenv, users, base_url, activity_url, db_config, recipients, subject, sender, mail_pwd
 
 	jenv = Environment(loader=PackageLoader("user_activity", 'templates'))
 	config = {}
@@ -30,13 +34,18 @@ def init_report():
 	base_url = config.get("base_url", "")
 	activity_url = config.get("activity_url", "")
 	db_config = config.get("db_config", {})
+	recipients = config.get("recipients", [])
+	subject = config.get("mail_subject", {})
+	sender = config.get("sender", {})
+	mail_pwd = pwd
 	
 	get_users_activity_records(base_url, users.keys())
 
-def format_date(str_datetime=None):
+def format_date(date_format='%Y-%m-%d', str_datetime=None, as_string=True, days=1):
 	date = None
 	date = datetime.now() if not str_datetime else datetime.strptime(str_datetime, "%Y-%m-%dT%H:%M:%S.%fZ")
-	return date.strftime("%Y-%m-%d")
+	date = date - timedelta(days=days)
+	return date.strftime(date_format) if as_string else date
 
 def get_user_actions(url):
 	response = requests.get(url)
@@ -60,7 +69,7 @@ def prepare_actions(user, actions):
 		if user not in [action.get("acting_username"), action.get("username")]:
 			continue
 
-		if format_date(action.get("created_at")) != format_date():
+		if format_date(str_datetime=action.get("created_at"), days=0) != format_date():
 			continue
 
 		key = action.get("slug")
@@ -78,7 +87,6 @@ def prepare_actions(user, actions):
 		report.update({
 			key: activities
 		})
-
 	return report
 
 def to_html(user, activity_report):
@@ -108,7 +116,12 @@ def get_parent(slug):
 
 def save_activity_report_to_db(user, activity_report):
 	def is_saved(parent, user, action):
-		return False
+		query = """ select id from `UserActivity` where user='%s' and topic_id='%s'
+					and post_number=%s"""%(user, action.get("topic_id"), action.get("post_number"))
+		if get_values(query, db_config):
+			return True
+		else:
+			return False
 
 	def add_activity(parent, user, action):
 		# check if activity already exists
@@ -133,6 +146,83 @@ def save_activity_report_to_db(user, activity_report):
 		parent = get_parent(slug)
 		[add_activity(parent, user, action) for action in actions]
 
+def get_activity_summery():
+	records = {}
+
+	def prepare_activity(result):
+		return {
+			slug: [{
+				"post_number": result.get("post_number"),
+				"created_at": result.get("created_at"),
+				"topic_id": result.get("topic_id"),
+				"title": result.get("title")
+			}]
+		}
+
+
+	query = """ select ua.*, t.slug from `UserActivity` ua inner join `Topics` t on t.id=ua.parent
+				where (created_at between "{start_date}" and "{end_date}")""".format(
+					start_date=format_date(days=7),
+					end_date=format_date(days=1)
+				)
+	results = get_values(query, db_config, as_dict=True)
+	if not results:
+		return {}
+
+	for result in results:
+		user = result.get("user")
+		slug = result.get("slug")
+
+		record = records.get(user, {})
+		if not record:
+			activity = prepare_activity(result)
+		elif record and not record.get(slug, {}):
+			current_activity = prepare_activity(result)
+			activity = records.get(user, {})
+			activity.update(current_activity)
+		else:
+			activity = records.get(user)
+			current_activity = prepare_activity(result)
+			old_activity = record.get(slug, [])
+			old_activity.extend(current_activity.get(slug))
+			activity.update({ slug: old_activity })
+
+		records.update({ user: activity })
+	return records
+
+def get_total_reply_count():
+	totals = {}
+	query = """ select user, count(id) as total from UserActivity where 
+				(created_at between "{start_date}" and "{end_date}") group by user""".format(
+					start_date=format_date(days=7),
+					end_date=format_date(days=1)
+				)
+	results = get_values(query, db_config, as_dict=True)
+	for result in results:
+		totals.update({ result.get("user"): result.get("total") })
+	return totals
+
+def mail_activity_report(user_wise_summary):
+
+	mail_content = jenv.get_template("mail_template.html").render({
+						"base_url": base_url,
+						"user_wise_summary": user_wise_summary,
+						"users": users,
+						"from_date": format_date(days=7),
+						"to_date": format_date(days=1),
+						"totals": get_total_reply_count()
+					})
+	mail_subject = subject.format(
+		from_date=format_date(date_format="%d-%b-%y", days=7),
+		to_date=format_date(date_format="%d-%b-%y", days=1)
+	)
+
+	active_users = [users.get(user).get("email") for user in user_wise_summary.keys()]
+	# recipients = active_users = ["makarand.b@indictranstech.com", "shraddha.r@indictranstech.com"]
+
+	send_mail(mail_pwd, recipients=recipients, sender=sender, 
+		subject=mail_subject, mail_content=mail_content, cc=active_users)
+
 def get_users_activity_records(base_url, users):
 	if not users:
 		return None
@@ -149,26 +239,21 @@ def get_users_activity_records(base_url, users):
 			activity_report = prepare_actions(user, actions)
 			save_activity_report_to_db(user, activity_report)
 
-			pdf_report_html, mail_report_html = to_html(user, activity_report)
-
-			if mail_report_html: user_wise_summary.append(mail_report_html)
-			# user_reports.update({
-			# 	user:{
-			# 		"pdf_report_html": pdf_report_html or "No Activity",
-			# 		"mail_report_html": mail_report_html or "No Activity"
-			# 	}
-			# })
 		except Exception, e:
 			import traceback
 			print e
 			print traceback.print_exc()
 
-	mail_content = jenv.get_template("mail_template.html").render({
-						"user_wise_summary": user_wise_summary
-					})
-
-	# with open("report.html", "w+") as file:
-	# 	file.write(mail_content)
+	try:
+		if format_date(date_format="%a") == "Sun":
+			print "sending mail"
+			user_wise_summary = get_activity_summery()
+			mail_activity_report(user_wise_summary)
+	except Exception, e:
+		raise e
 
 if __name__ == "__main__":
-	init_report()
+	if len(sys.argv) == 2:
+		init_report(sys.argv[1])
+	else:
+		print "missing parameter"
